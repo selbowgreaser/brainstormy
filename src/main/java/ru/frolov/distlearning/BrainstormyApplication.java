fe9adfd694a0593627110629f3a68a3083f63161
selbowgreaser <selbowgreaser@gmail.com>
2023-04-19 23:09:24 +0300

    Added app

---
+++ src/main/java/ru/frolov/distlearning/BrainstormyApplication.java
@@ -0,0 +1,13 @@
+package ru.frolov.distlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainstormyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainstormyApplication.class, args);
+    }
+
+}
