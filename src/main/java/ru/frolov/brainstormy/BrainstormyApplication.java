80122a4db5662557839e2d06bf0730b638926695
selbowgreaser <selbowgreaser@gmail.com>
2023-04-21 17:12:31 +0300

    Added bean of model mapper

--- src/main/java/ru/frolov/brainstormy/BrainstormyApplication.java
+++ src/main/java/ru/frolov/brainstormy/BrainstormyApplication.java
@@ -1,13 +1,20 @@
 package ru.frolov.brainstormy;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BrainstormyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BrainstormyApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
