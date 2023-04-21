2ac41417cb742aa5cc98d76f4be50173e489c153
selbowgreaser <selbowgreaser@gmail.com>
2023-04-21 15:31:17 +0300

    Added details for user security

---
+++ src/main/java/ru/frolov/brainstormy/security/UserDetails.java
@@ -0,0 +1,54 @@
+package ru.frolov.brainstormy.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import ru.frolov.brainstormy.model.User;
+
+import java.util.Collection;
+
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
+
+    private final User user;
+
+    public UserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
