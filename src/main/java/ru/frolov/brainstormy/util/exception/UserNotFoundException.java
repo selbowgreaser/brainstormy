9f8cce70eb7a202c5171466ed4ff9aa8e0235eb3
selbowgreaser <selbowgreaser@gmail.com>
2023-04-21 17:13:01 +0300

    Added user id to UserNotFoundException constructor

--- src/main/java/ru/frolov/brainstormy/util/exception/UserNotFoundException.java
+++ src/main/java/ru/frolov/brainstormy/util/exception/UserNotFoundException.java
@@ -1,5 +1,15 @@
 package ru.frolov.brainstormy.util.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserNotFoundException extends RuntimeException {
 
+    private Integer userId;
+
+    public UserNotFoundException(Integer userId) {
+        this.userId = userId;
+    }
 }
