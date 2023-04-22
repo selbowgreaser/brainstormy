190649a9f9c3a773d70d5f0da92cb48e965ecec8
selbowgreaser <selbowgreaser@gmail.com>
2023-04-22 17:01:54 +0300

    Deleted @Setter

--- src/main/java/ru/frolov/brainstormy/util/exception/PersonNotFoundException.java
+++ src/main/java/ru/frolov/brainstormy/util/exception/PersonNotFoundException.java
@@ -1,13 +1,11 @@
 package ru.frolov.brainstormy.util.exception;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class PersonNotFoundException extends RuntimeException {
 
-    private Integer userId;
+    private final Integer userId;
 
     public PersonNotFoundException(Integer userId) {
         this.userId = userId;
