3abd74ca3a6d1224db5dce0882d6db36c2b927e7
selbowgreaser <selbowgreaser@gmail.com>
2023-04-22 14:57:43 +0300

    Added method to determine the role

--- src/main/java/ru/frolov/brainstormy/service/PersonService.java
+++ src/main/java/ru/frolov/brainstormy/service/PersonService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.frolov.brainstormy.model.Person;
+import ru.frolov.brainstormy.model.Role;
 import ru.frolov.brainstormy.repository.UserRepository;
 import ru.frolov.brainstormy.util.exception.PersonNotFoundException;
 
@@ -38,6 +39,20 @@ public class PersonService {
 
     public Boolean isPhoneExist(String phone) {
         return userRepository.findByPhone(phone).isPresent();
+    }
+
+    public Role getRoleById(Integer id) {
+        Person person = findOne(id);
+
+        return defineRole(person);
+    }
+
+    private Role defineRole(Person person) {
+        if (person.getAdmin() != null) {
+            return Role.ADMIN;
+        } else if (person.getTeacher() != null) {
+            return Role.TEACHER;
+        } else return Role.STUDENT;
     }
 
 }
