d2e26d4573fcbe4ce0e7c98c153363a2c8c6f186
selbowgreaser <selbowgreaser@gmail.com>
2023-04-24 09:05:54 +0300

    Added super with default message and userLogin field for exception constructor

--- src/main/java/ru/frolov/brainstormy/util/exception/PersonNotFoundException.java
+++ src/main/java/ru/frolov/brainstormy/util/exception/PersonNotFoundException.java
@@ -5,9 +5,16 @@ import lombok.Getter;
 @Getter
 public class PersonNotFoundException extends RuntimeException {
 
-    private final Integer userId;
+    private Integer userId;
+    private String userLogin;
 
     public PersonNotFoundException(Integer userId) {
+        super("User with id " + userId + "not found");
         this.userId = userId;
+    }
+
+    public PersonNotFoundException(String userLogin) {
+        super("User with login " + userLogin + "not found");
+        this.userLogin = userLogin;
     }
 }
