640b397a04601ece5b1947bc9a1e0336c39dd06e
selbowgreaser <selbowgreaser@gmail.com>
2023-04-24 09:06:43 +0300

    Added controller for courses

---
+++ src/main/java/ru/frolov/brainstormy/controller/CourseController.java
@@ -0,0 +1,27 @@
+package ru.frolov.brainstormy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.frolov.brainstormy.service.CourseService;
+
+@Controller
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("courses", courseService.findAll());
+
+        return "course/index";
+    }
+}
